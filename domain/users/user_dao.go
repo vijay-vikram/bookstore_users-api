87a03a0bcfc4da85765423252b1e242a35468132
Vijay Vikram <vijay.vikram@grabtaxi.com>
2020-04-28 00:14:30 +0530

    Adding functionality to Save User in database and Get User from database

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,8 +2,17 @@ package users
 
 import (
 	"fmt"
+	"github.com/vijay-vikram/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/vijay-vikram/bookstore_users-api/utils/dates"
 	"github.com/vijay-vikram/bookstore_users-api/utils/errors"
+	"strings"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	errorNoRow       = "no rows in result set"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
 
 var (
@@ -11,24 +20,49 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
-	results := usersDB[user.Id]
-	if results == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("User %d not found ", user.Id))
+
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	user.Id = results.Id
-	user.Email = results.Email
-	user.LastName = results.LastName
-	user.FirstName = results.FirstName
-	user.DateCreated = results.DateCreated
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.Id)
+
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorNoRow) {
+			return errors.NewNotFoundError(fmt.Sprintf("User %d not found", user.Id))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to Get user %d  %s", user.Id, err.Error()))
+	}
 
 	return nil
 }
 
 func (user *User) Save() *errors.RestErr {
-	if usersDB[user.Id] != nil {
-		return errors.NewBadRequestError(fmt.Sprintf("User %d already exits", user.Id))
+
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
+
 	user.DateCreated = dates.GetNoWString()
-	usersDB[user.Id] = user
+
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("Email %s already exits : ", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user  %s", err.Error()))
+	}
+
+	userID, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user  %s", err.Error()))
+	}
+
+	user.Id = userID
+
 	return nil
 }
--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -26,4 +26,12 @@ func NewNotFoundError(message string) *RestErr {
 	}
 }
 
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+}
+
 
