9cf0be21878f23147a6437b9987e5f37d29bc438
Vijay Vikram <vijay.vikram@grabtaxi.com>
2020-04-28 00:49:48 +0530

    Adding util methods to parse mysql error

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,16 +1,13 @@
 package users
 
 import (
-	"fmt"
 	"github.com/vijay-vikram/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/vijay-vikram/bookstore_users-api/utils/dates"
 	"github.com/vijay-vikram/bookstore_users-api/utils/errors"
-	"strings"
+	"github.com/vijay-vikram/bookstore_users-api/utils/mysql"
 )
 
 const (
-	indexUniqueEmail = "email_UNIQUE"
-	errorNoRow       = "no rows in result set"
 	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
@@ -29,11 +26,8 @@ func (user *User) Get() *errors.RestErr {
 
 	result := stmt.QueryRow(user.Id)
 
-	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
-		if strings.Contains(err.Error(), errorNoRow) {
-			return errors.NewNotFoundError(fmt.Sprintf("User %d not found", user.Id))
-		}
-		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to Get user %d  %s", user.Id, err.Error()))
+	if getErr := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); getErr != nil {
+		return mysql.ParseError(getErr)
 	}
 
 	return nil
@@ -49,17 +43,14 @@ func (user *User) Save() *errors.RestErr {
 
 	user.DateCreated = dates.GetNoWString()
 
-	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
-	if err != nil {
-		if strings.Contains(err.Error(), indexUniqueEmail) {
-			return errors.NewBadRequestError(fmt.Sprintf("Email %s already exits : ", user.Email))
-		}
-		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user  %s", err.Error()))
+	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if saveErr != nil {
+		return mysql.ParseError(saveErr)
 	}
 
 	userID, err := insertResult.LastInsertId()
 	if err != nil {
-		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user  %s", err.Error()))
+		return mysql.ParseError(saveErr)
 	}
 
 	user.Id = userID
---
+++ utils/mysql/mysql_utils.go
@@ -0,0 +1,31 @@
+package mysql
+
+import (
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/vijay-vikram/bookstore_users-api/utils/errors"
+	"strings"
+)
+
+const (
+	errorNoRow = "no rows in result set"
+)
+
+func ParseError(error error) *errors.RestErr {
+
+	sqlError, ok := error.(*mysql.MySQLError)
+
+	if !ok {
+		if strings.Contains(error.Error(), errorNoRow) {
+			return errors.NewNotFoundError(fmt.Sprintf("no record mtching given id"))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error parsing database response"))
+	}
+
+	switch sqlError.Number {
+	case 1062:
+		return errors.NewBadRequestError(fmt.Sprintf("Invalid data"))
+	}
+
+	return errors.NewInternalServerError(fmt.Sprintf("error processing request"))
+}
